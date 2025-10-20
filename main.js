4f5b8d3b2df9fd73c2590b126f200646dd007cff
Качановський Владислав bc2025-4 <kacanovskijvlad@gmail.com>
2025-10-20 03:19:45 +0300

    installed new module fast-xml-parser and updates main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const http = require('http');
 const commander = require('commander');
+const url = require('url');
+const { XMLBuilder } = require('fast-xml-parser');
 
 const program = commander.program;
 program
@@ -16,8 +18,54 @@ if (!fs.existsSync(options.input)) {
     process.exit(1); 
 }
 const server = http.Server(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' });
-    response.end('Сервер запущено test 2 ');
+    const parsedUrl = url.parse(request.url, true);
+    const query = parsedUrl.query;
+    const mfo = query.mfo === 'true';
+    const normal = query.normal === 'true';
+    fs.readFile(options.input, 'utf8', function(err, data) {
+        if (err) {
+            response.writeHead(500, { 'Content-Type': 'text/plain; charset=utf-8' });
+            response.end('Помилка читання файлу');
+            return;
+        }
+            let banks = JSON.parse(data);
+            if (normal) {
+                const FilterNormal = [];
+                for (const bank of banks) {
+                    if (bank.COD_STATE === 1) {
+                        FilterNormal.push(bank);
+                    }
+                }
+                banks = FilterNormal;
+            }
+            const results = [];
+            for (const bank of banks) {
+                let result = {};
+                if (mfo) {
+                    result.mfo = bank.MFO;
+                }
+                result.name = bank.FULLNAME || bank.SHORTNAME || undefined;
+                result.namestate = bank.NAME_STATE;
+                result.stetecode = bank.COD_STATE;
+                result.firstname = bank.FIRST_NAME || undefined;
+                result.posada = bank.NAME_DOLGN || undefined;
+                results.push(result);
+            }
+            const builder = new XMLBuilder();
+            const XMLObj = {
+                banks: {
+                    bank: results
+                }
+            };
+            const XMlresult = builder.build(XMLObj);
+            fs.writeFile('output.xml', XMlresult, function(err) {
+                if (err) {
+                    console.error('Помилка запису файлу');
+                }
+            });
+            response.writeHead(200, { 'Content-Type': 'application/xml; charset=utf-8' });
+            response.end(XMlresult);
+    });
 });
 
 server.listen(options.port, options.host, function() {
